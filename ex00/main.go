2565be3444b48a07600c8274faef53003e59d661
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-25 14:48:36 +0900

    [Add]argument validation

--- ex00/main.go
+++ ex00/main.go
@@ -11,8 +11,13 @@ func main() {
 	args := flag.Args()
 	if len(args) == 0 {
 		fmt.Fprintln(os.Stderr, "error: invalide argument")
-		os.Exit(1)
+		os.Exit(0)
 	}
-	fmt.Println(args)
+	for _, path := range args {
+		if _, err := os.Stat(path); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s: no such file or directory\n", path)
+			os.Exit(0)
+		}
+	}
 }
 
