12c66c377a4494ae17063a67c46db32de2355842
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-27 02:46:50 +0900

    [Add]Convert jpg to png

--- ex00/main.go
+++ ex00/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
 	"flag"
 	"path/filepath"
-	//"image/png"
+	"image/png"
+	"image/jpeg"
 	"strings"
 )
 
@@ -22,11 +24,33 @@ func main() {
 				fmt.Fprintf(os.Stderr, "error: %s: no such file or directory\n", dir)
 				return err
 			}
-			//fmt.Println(path);
-			if info.IsDir() || !strings.HasSuffix(info.Name(), ".jpg") {
+			if info.IsDir() || !strings.HasSuffix(path, ".jpg") {
 				return nil
 			}
-			fmt.Println(path);
+			jpg_path := path
+			png_path := strings.Replace(path, ".jpg", ".png", 1)
+			jpg_file, err := os.Open(jpg_path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			jpg_img, err := jpeg.Decode(jpg_file)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if err := jpg_file.Close(); err != nil {
+				log.Fatal(err)
+			}
+			png_file, err := os.Create(png_path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if err := png.Encode(png_file, jpg_img); err != nil {
+				png_file.Close()
+				log.Fatal(err)
+			}
+			if err := png_file.Close(); err != nil {
+				log.Fatal(err)
+			}
 			return nil
 		})
 	}
