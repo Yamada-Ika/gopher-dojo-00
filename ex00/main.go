863424bd81b621811634222c344e2731d1653c20
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-27 01:44:37 +0900

    [Fix]can explore recursively

--- ex00/main.go
+++ ex00/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"flag"
+	"path/filepath"
 )
 
 func main() {
@@ -13,11 +14,15 @@ func main() {
 		fmt.Fprintln(os.Stderr, "error: invalide argument")
 		os.Exit(0)
 	}
-	for _, path := range args {
-		if _, err := os.Stat(path); err != nil {
-			fmt.Fprintf(os.Stderr, "error: %s: no such file or directory\n", path)
-			os.Exit(0)
-		}
+	for _, dir := range args {
+		filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error: %s: no such file or directory\n", dir)
+				return err
+			}
+			fmt.Println(path);
+			return nil
+		})
 	}
 }
 
