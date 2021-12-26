e4151af2024280c0a7269e9c5497bc041e7a900b
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-27 02:50:04 +0900

    [Add]Display error message in case of not jpeg file

--- ex00/main.go
+++ ex00/main.go
@@ -21,10 +21,14 @@ func main() {
 	for _, dir := range args {
 		filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "error: %s: no such file or directory\n", dir)
+				fmt.Fprintf(os.Stderr, "error: %s: no such file or directory\n", path)
 				return err
 			}
-			if info.IsDir() || !strings.HasSuffix(path, ".jpg") {
+			if info.IsDir() {
+				return nil
+			}
+			if !strings.HasSuffix(path, ".jpg") {
+				fmt.Fprintf(os.Stderr, "error: %s is not a valid file\n", path)
 				return nil
 			}
 			jpg_path := path
