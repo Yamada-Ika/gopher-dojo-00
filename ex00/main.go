73347b00c06f37a1882e7a1d890e1c3aa070b06c
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-31 14:56:48 +0900

    [Fix]Add convert function

--- ex00/main.go
+++ ex00/main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"io/fs"
 	"fmt"
 	"image/jpeg"
 	"image/png"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+func convert(path string) error {
+	if !strings.HasSuffix(path, ".jpg") {
+		return errors.New(fmt.Sprintf("error: %s is not a valid file\n", path))
+	}
+	jpg_path := path
+	png_path := strings.Replace(path, ".jpg", ".png", 1)
+	jpg_file, err := os.Open(jpg_path)
+	if err != nil {
+		return err
+	}
+	jpg_img, err := jpeg.Decode(jpg_file)
+	if err != nil {
+		return err
+	}
+	if err := jpg_file.Close(); err != nil {
+		return err
+	}
+	png_file, err := os.Create(png_path)
+	if err != nil {
+		return err
+	}
+	if err := png.Encode(png_file, jpg_img); err != nil {
+		png_file.Close()
+		return err
+	}
+	if err := png_file.Close(); err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	flag.Parse()
@@ -28,33 +59,8 @@ func main() {
 			if info.IsDir() {
 				return nil
 			}
-			if !strings.HasSuffix(path, ".jpg") {
-				fmt.Fprintf(os.Stderr, "error: %s is not a valid file\n", path)
-				return nil
-			}
-			jpg_path := path
-			png_path := strings.Replace(path, ".jpg", ".png", 1)
-			jpg_file, err := os.Open(jpg_path)
-			if err != nil {
-				log.Fatal(err)
-			}
-			jpg_img, err := jpeg.Decode(jpg_file)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if err := jpg_file.Close(); err != nil {
-				log.Fatal(err)
-			}
-			png_file, err := os.Create(png_path)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if err := png.Encode(png_file, jpg_img); err != nil {
-				png_file.Close()
-				log.Fatal(err)
-			}
-			if err := png_file.Close(); err != nil {
-				log.Fatal(err)
+			if err := convert(path); err != nil {
+				return errors.New("faile : convert")
 			}
 			return nil
 		})
