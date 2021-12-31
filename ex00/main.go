4527db164ebc04788219bdedd4357bb0d110482e
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-31 15:14:37 +0900

    [Fix]rename vals

--- ex00/main.go
+++ ex00/main.go
@@ -12,39 +12,39 @@ import (
 	"strings"
 )
 
+var iFlag = flag.String("i", "jpg", "input file extension")
+var oFlag = flag.String("o", "png", "output file extension")
+
 func convert(path string) error {
 	if !strings.HasSuffix(path, ".jpg") {
 		return errors.New(fmt.Sprintf("error: %s is not a valid file\n", path))
 	}
-	jpg_path := path
-	png_path := strings.Replace(path, ".jpg", ".png", 1)
-	jpg_file, err := os.Open(jpg_path)
+	in_path := path
+	out_path := strings.Replace(path, ".jpg", ".png", 1)
+	in_file, err := os.Open(in_path)
 	if err != nil {
 		return err
 	}
-	jpg_img, err := jpeg.Decode(jpg_file)
+	in_img, err := jpeg.Decode(in_file)
 	if err != nil {
 		return err
 	}
-	if err := jpg_file.Close(); err != nil {
+	if err := in_file.Close(); err != nil {
 		return err
 	}
-	png_file, err := os.Create(png_path)
+	out_file, err := os.Create(out_path)
 	if err != nil {
 		return err
 	}
-	if err := png.Encode(png_file, jpg_img); err != nil {
-		png_file.Close()
+	if err := png.Encode(out_file, in_img); err != nil {
+		out_file.Close()
 		return err
 	}
-	if err := png_file.Close(); err != nil {
+	if err := out_file.Close(); err != nil {
 		return err
 	}
 	return nil
 }
-
-var iFlag = flag.String("i", "jpg", "input file extension")
-var oFlag = flag.String("o", "png", "output file extension")
 
 func main() {
 	flag.Parse()
